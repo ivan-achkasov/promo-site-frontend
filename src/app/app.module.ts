86d0e808927cc03ecac6fb6b8770ded81c7fd943
ivana <ivan.achkasov.ua@gmail.com>
2022-06-04 16:53:57 +0300

    Removed hash in routes and removed unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,13 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileModule } from './profile/profile.module';
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ProfileComponent
-  }
+  { path: '', component: ProfileComponent }
 ];
-
-const config: ExtraOptions = {
-  useHash: true,
-};
 
 @NgModule({
   declarations: [
@@ -25,11 +16,8 @@ const config: ExtraOptions = {
   ],
   imports: [
     BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(routes, config),
-    ProfileModule,
-    NgbModule
+    RouterModule.forRoot(routes),
+    ProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
