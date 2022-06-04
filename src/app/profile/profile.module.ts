cd9cc174f3988825d002ccdeb951b8a57f1a3114
ivana <ivan.achkasov.ua@gmail.com>
2022-06-04 15:56:28 +0300

    Removed about component

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -4,7 +4,6 @@ import { ProfileComponent } from './profile.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { IntroComponent } from './intro/intro.component';
-import { AboutComponent } from './about/about.component';
 import { EducationComponent } from './education/education.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -16,13 +15,13 @@ import { SkillsComponent } from './skills/skills.component';
   ],
   declarations: [
     ProfileComponent,
+
     HeaderComponent,
-    FooterComponent,
-    AboutComponent,
     IntroComponent,
-    EducationComponent,
-    ExperienceComponent,
     SkillsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    FooterComponent,
   ],
   providers: []
 })
